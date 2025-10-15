2835994be2b3e097b8d16e9464bae87d63a190e2
Gilberto Romanhole <gilbertoromanhole@gmail.com>
2025-10-15 16:57:21 -0300

    Update ipFilter.js

--- src/middlewares/ipFilter.js
+++ src/middlewares/ipFilter.js
@@ -3,7 +3,7 @@ import { allowedIPs } from '../config/allowedIPs.js';
 // Middleware para bloquear requisições de IPs não autorizados
 export const ipFilter = (req, res, next) => {
     if (!allowedIPs.includes(req.ip)) {
-        return res.status(403).json({ error: 'Pare de tentar hackear! ;)' });
+        return res.status(403).json({ error: 'Amigo(a), pare de tentar hackear! Grato! ;)' });
     }
     next();
 };
